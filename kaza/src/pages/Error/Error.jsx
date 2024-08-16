eb2db66ea82ea6ad88995bd7526bf4411a5bd2ab
Samiya <samii-1989@live.com>
2024-08-16 12:39:27 +0200

    "Error page"

--- kaza/src/pages/Error/Error.jsx
+++ kaza/src/pages/Error/Error.jsx
@@ -1,9 +1,24 @@
 
+import Footer from './../../components/Footer/Footer';
+import Navigation from './../../components/Navigation/Navigation';
+import { Link } from 'react-router-dom';
+import style from './Error.module.css'
+import error from '../../assets/404.png'
+import Container from '../../components/Container/Container';
+
 const Error = () => {
   return (
-    <div>
-      Error
-    </div>
+    <>
+      <Navigation />
+        <Container>
+          <div className={style.error}>
+              <img src={error} alt=""  className={style.errorImg}/>
+              <h2  className={style.errorTitle}>Oups! La page que vous demandez n&apos;existe pas.</h2>
+              <Link  className={style.errorLink} to="/">Retournez sur la page d&apos;accueil</Link>
+          </div>
+        </Container>
+      <Footer />
+    </>
   )
 }
 
