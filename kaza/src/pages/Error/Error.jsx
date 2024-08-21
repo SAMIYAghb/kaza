005eee75916e2c114d3fc3fe2c3ac68e33a5ec1f
Samiya <samii-1989@live.com>
2024-08-21 11:21:58 +0200

    "Error page responsive"

--- kaza/src/pages/Error/Error.jsx
+++ kaza/src/pages/Error/Error.jsx
@@ -7,7 +7,7 @@ const Error = () => {
   return (
     <>
           <div className={style.error}>
-              <img src={error} alt=""  className={style.errorImg}/>
+              <img src={error} alt="error image"  className={style.errorImg}/>
               <h2  className={style.errorTitle}>Oups! La page que vous demandez n&apos;existe pas.</h2>
               <Link  className={style.errorLink} to="/">Retournez sur la page d&apos;accueil</Link>
           </div>
